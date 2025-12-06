c845f08f717702825131a8c992873378c2406738
Wolpi066 <volpinoemiliano@gmail.com>
2025-12-05 22:50:23 -0300

    Correccion de comentarios

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include "raylib.h"
 #include "Juego.h"
 #include "SistemaArchivos.h"
-// ¡Necesitamos incluir "Constantes.h" para inicializar la ventana!
+
 #include "Constantes.h"
 
 int main() {
@@ -10,17 +10,14 @@ int main() {
     // 1. Inicializar la ventana de Raylib
     InitWindow(Constantes::ANCHO_PANTALLA, Constantes::ALTO_PANTALLA, "Noche Movidita");
 
-    // --- ¡¡CAMBIO CRÍTICO!! Inicializar el dispositivo de Audio ---
     // Esto es obligatorio para que funcionen LoadSound/LoadMusicStream
     InitAudioDevice();
     // -------------------------------------------------------------
 
-    SetTargetFPS(60); // Buena práctica
-    HideCursor();     // Oculta el cursor del sistema.
+    SetTargetFPS(60);
+    HideCursor();
 
-    // 2. Crear la instancia del juego
-    // Esto llamará al constructor de Juego, que prepara el mapa, gestor y CARGA LOS SONIDOS.
-    // Por eso InitAudioDevice() tiene que estar ANTES de esta línea.
+
     Juego miJuego;
 
     // 3. Ejecutar el juego
@@ -28,7 +25,7 @@ int main() {
 
     // 4. Limpieza
     // Cerrar el dispositivo de audio antes de la ventana
-    CloseAudioDevice(); // <--- ¡AÑADIDO!
+    CloseAudioDevice();
 
     CloseWindow();
     return 0;
